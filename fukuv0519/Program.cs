69ab897fd332ada70c86bedc6360d02fea00a76b
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-05-19 13:31:00 +0900

    Initial commit

---
+++ fukuv0519/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fukuv0519
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
